f4c5a02db142902bd29de7f06b9bc5cc1028067a
ilhamprabuzaky <ilhamprabuzakys@gmail.com>
2020-12-03 11:24:15 +0700

    initial commit

---
+++ KKSI/testing-lib/numpy-lib.py
@@ -0,0 +1,23 @@
+""" NUMPY IMPORT TESTER  """
+import numpy as np
+
+# a = np.array([1, 2, 3])
+# b = [1, 2, 3]
+
+# print('Before Added New Element\n===========================')
+# print(a)
+# print(b)
+
+# print('After Added New Element\n===========================')
+# a += 1
+# b += [1]
+# print(a)
+# print(b)
+
+""" Aritmatika """
+# # MATRIKS or MD Array
+a = np.array(([1, 2, 3],[4, 5, 6]))
+b = np.array(([7, 8, 9],[-1, -2, -3]))
+# hasil = a + b
+hasil = a * b
+print(hasil)
